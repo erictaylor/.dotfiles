5943419ac3a7be2b02b5e434499ef57e6febf376
Eric Taylor <eric@daxos.com>
2014-06-14 11:37:00 -0600

    Fixed hard coded path to home.

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -1,1 +1,1 @@
-export PATH=/Users/erictaylor/.dotfiles/bin:/usr/local/bin:/usr/local/sbin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/X11/bin:$PATH
+export PATH=$HOME/.dotfiles/bin:/usr/local/bin:/usr/local/sbin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/X11/bin:$PATH
