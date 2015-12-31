6c64d969b32210532c09dca1d9c4cc83b92d3196
Eric Taylor <eric@daxos.com>
2015-12-31 12:07:08 -0700

    Update dot files biatch

--- zsh/aliase.zsh
+++ zsh/aliase.zsh
@@ -2,6 +2,12 @@
 alias reload!='source ~/.zshrc'
 alias re!='reload!'
 
+# GCC
+# alias gcc='gcc-4.9'
+# alias cc='gcc-4.9'
+# alias g++='g++-4.9'
+# alias c++='c++-4.9'
+
 # IP ADDRESSES
 alias ip="dig +short myip.opendns.com @resolver1.opendns.com"
 alias localip="ipconfig getifaddr en1"
